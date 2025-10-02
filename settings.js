dacbdaeaec6dc540a8ad7d4753ef452c370105ec
mabacgahan-bit <mabacgahan@gmail.com>
2025-10-02 07:35:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=011604facefb4f56' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=020204228bff364a' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "52WCEZrBt6vwA6rBprswvgkdFKg2Gl29ssGt" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
